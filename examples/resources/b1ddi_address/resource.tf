278cacac693dba8b98ea1869e2e2e21835905cf0
Aravintha Kumar <akumar3@infoblox.com>
2023-10-04 19:59:55 +0530

    Validating resource in update. Added ignore for NA IP examples

--- examples/resources/b1ddi_address/resource.tf
+++ examples/resources/b1ddi_address/resource.tf
@@ -48,6 +48,11 @@ resource "b1ddi_address" "next_available_subnet_ip" {
   address  = b1ddi_subnet.example_tf_subnet.id
   comment = "Example Address automatically allocated from the subnet"
   space   = b1ddi_ip_space.example_tf_space.id
+  lifecycle {
+    ignore_changes = [
+      address,
+    ]
+  }
 }
 
 # Address can be allocated from a Range via the parent field
@@ -55,6 +60,12 @@ resource "b1ddi_address" "next_available_range_ip" {
   address =  b1ddi_range.tf_acc_test_range.id
   comment = "Example Address automatically allocated from the Range"
   space   = b1ddi_ip_space.example_tf_space.id
+  lifecycle {
+    ignore_changes = [
+      address,
+      range,
+    ]
+  }
 }
 
 # Address can be allocated from a Address block via the parent field
@@ -62,4 +73,10 @@ resource "b1ddi_address" "next_available_address_block_ip" {
   address = b1ddi_address_block.tf_example_address_block.id
   comment = "Example Address automatically allocated from the address_block"
   space   = b1ddi_ip_space.example_tf_space.id
+  lifecycle {
+    ignore_changes = [
+      address,
+    ]
+  }
 }
+
